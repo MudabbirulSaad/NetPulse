0833470776b910b9fa9cf540805f085ca4028cef
Mudabbirul Saad <mudabbirulsaad@gmail.com>
2026-08-16 16:19:31 +1000

    feat(sample): add Task 1.1 desktop application
    Refs: NP-015

--- task-1.1/SampleApp/Program.cs
+++ task-1.1/SampleApp/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Deployment Activity 1: Pass Task Completed!");
+Console.WriteLine("Press any key to exit...");
+
+if (!Console.IsInputRedirected)
+{
+    Console.ReadKey(intercept: true);
+}
